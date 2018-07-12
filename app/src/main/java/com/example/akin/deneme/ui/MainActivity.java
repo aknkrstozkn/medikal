fbe5bd85e359052c38874353ba516ed09ffaceb8
U140709037 <bincalibre@gmail.com>
2018-07-12 18:01:45 +0300

    Program's main-activity.

--- app/src/main/java/com/example/akin/deneme/ui/MainActivity.java
+++ app/src/main/java/com/example/akin/deneme/ui/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.akin.deneme.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.akin.deneme.R;
+import com.example.akin.deneme.core.DataBase;
 
 public class MainActivity extends AppCompatActivity {
-
+    DataBase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new DataBase(getApplicationContext());
+
+        final Button butOutOfDatePres = findViewById(R.id.buttonOutOfDatePres);
+        int presAmount = db.getOutOfDatePres().size();
+        butOutOfDatePres.setText(butOutOfDatePres.getText() + String.valueOf(presAmount) + " )");
+
+        butOutOfDatePres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddSaleActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
+
 
 }
