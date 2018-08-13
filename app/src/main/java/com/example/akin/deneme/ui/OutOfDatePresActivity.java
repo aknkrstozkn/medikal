b9b9e50dc874a6fc1ce2f54a6634c55db5b708f6
U140709037 <bincalibre@gmail.com>
2018-08-13 17:31:44 +0300

    For now, these classes are done.

--- app/src/main/java/com/example/akin/deneme/ui/OutOfDatePresActivity.java
+++ app/src/main/java/com/example/akin/deneme/ui/OutOfDatePresActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.akin.deneme.R;
 import com.example.akin.deneme.core.DataBase;
@@ -42,87 +43,17 @@ public class OutOfDatePresActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
-        Relative relative;
-        List<Relativity> relativities;
-        Patient patient;
-        Product product;
-        List<ProductAmount> productAmounts;
-        Prescription prescription;
-        Sale sale;
-        relative = new Relative();
-        relativities = new ArrayList<>();
-        patient = new Patient();
-        product = new Product();
-        productAmounts = new ArrayList<>();
-        prescription = new Prescription();
-        sale = new Sale();
 
-        relative.setName("Akın Kürşat Özkan");
-        relative.setTc("16192221284");
-        relative.setAddress("Kötekli Mahallesi");
-        relative.setPhoneNumber("05458521996");
-        relative.setCordinate("dasdas");
-
-        Relativity relativity = new Relativity();
-        relativity.setRelative(relative);
-        relativity.setRelativity("Oğlu");
-
-        relativities.add(relativity);
-
-        patient.setName("ORHAN ÖZKAN");
-        patient.setTc("16195221066");
-        patient.setAddress("Müminli Mahallesi");
-        patient.setPhoneNumber("05323036820");
-        patient.setRelatives(relativities);
-        patient.setCordinate("asdasd");
-
-        product.setType("Bez");
-        product.setSerialNumber("asdasd");
-        product.setBarCode("asdasds");
-
-        ProductAmount productAmount = new ProductAmount();
-        productAmount.setProduct(product);
-        productAmount.setProductAmount(150);
-
-        productAmounts.add(productAmount);
-
-        Calendar calendar = Calendar.getInstance();
-
-        prescription.seteDate(calendar.getTimeInMillis());
-        prescription.setsDate(calendar.getTimeInMillis());
-        prescription.setPrescriptionsProductList(productAmounts);
-
-        sale.setRelative(relative);
-        sale.setPatient(patient);
-        sale.setDate(calendar.getTimeInMillis());
-        sale.setPrescription(prescription);
-        List<Sale> sales = new ArrayList<>();
-        sales.add(sale);
-        mAdapter = new MyAdapter(db.getSales());
-        mRecyclerView.setAdapter(mAdapter);
-
-
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Dikkat!");
-        builder.setMessage(String.valueOf(db.get()));
-        builder.setNegativeButton("Hayır", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int id) {
-
-
-
-            }
-        });
-
-
-        builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-
-
-            }
-        });
-
-
-        builder.show();
+        try {
+            mAdapter = new MyAdapter(db.getOutOfDatePresSales());
+            mRecyclerView.setAdapter(mAdapter);
+        }catch (NullPointerException e){
+            Toast errorToast = Toast.makeText(this, "Herhangi bir reçete satışı bulunmamaktadır", Toast.LENGTH_SHORT);
+            errorToast.show();
+        }
+        catch (Exception e){
+            throw e;
+        }
 
     }
 
--- app/src/main/java/com/example/akin/deneme/ui/PrescriptionListActivity.java
+++ app/src/main/java/com/example/akin/deneme/ui/PrescriptionListActivity.java
@@ -2,14 +2,40 @@ package com.example.akin.deneme.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.akin.deneme.R;
+import com.example.akin.deneme.core.DataBase;
 
 public class PrescriptionListActivity extends AppCompatActivity {
-
+    DataBase db;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prescription_list);
+
+        db = new DataBase(this);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        try {
+            mAdapter = new MyAdapter(db.getInDatePresSales());
+            mRecyclerView.setAdapter(mAdapter);
+        }catch (NullPointerException e){
+            Toast errorToast = Toast.makeText(this, "Herhangi bir reçete satışı bulunmamaktadır", Toast.LENGTH_SHORT);
+            errorToast.show();
+        }
+        catch (Exception e){
+            throw e;
+        }
     }
 }
