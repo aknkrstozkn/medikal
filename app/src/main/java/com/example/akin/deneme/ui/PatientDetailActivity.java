60341bae9e89981f92bfd3211f2cbb797e7f8512
U140709037 <bincalibre@gmail.com>
2018-09-17 10:08:57 +0300

    button patient-call and button gps are functional now.

--- app/src/main/java/com/example/akin/deneme/ui/PatientDetailActivity.java
+++ app/src/main/java/com/example/akin/deneme/ui/PatientDetailActivity.java
@@ -1,11 +1,17 @@
 package com.example.akin.deneme.ui;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.akin.deneme.R;
@@ -41,7 +47,7 @@ public class PatientDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         String id = intent.getStringExtra("tc");
-        Patient patient = db.getPatient(id);
+        final Patient patient = db.getPatient(id);
 
         TextView textViewPatientName = findViewById(R.id.textViewPatientName);
         textViewPatientName.setText(patient.getName());
@@ -51,6 +57,25 @@ public class PatientDetailActivity extends AppCompatActivity {
 
         TextView textViewPatientPhoneNumber = findViewById(R.id.textViewPatientPhoneNumber);
         textViewPatientPhoneNumber.setText("Tel: " + patient.getPhoneNumber());
+        textViewPatientPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = patient.getPhoneNumber();
+                if (number.isEmpty())
+                    return;
+                String uri = "tel:0" + number;
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                try {
+                    intent.setData(Uri.parse(uri));
+                }catch (Exception e){
+                    return;
+                }
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                context.startActivity(intent);
+            }
+        });
 
         TextView textViewPatientAddress = findViewById(R.id.textViewPatientAddress);
         textViewPatientAddress.setText(patient.getAddress());
@@ -64,5 +89,20 @@ public class PatientDetailActivity extends AppCompatActivity {
 
         mAdapter = new MyAdapter(patient, context);
         mRecyclerView.setAdapter(mAdapter);
+
+        Button buttonGPS =  findViewById(R.id.buttonPatientGPS);
+        final Uri gmmIntentUri = Uri.parse("google.navigation:q=" + patient.getCordinate());
+        buttonGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                Intent i = Intent.createChooser(mapIntent, "choose");
+                startActivity(i);
+
+            }
+        });
+
+        if(patient.getCordinate() == null)
+            buttonGPS.setVisibility(View.INVISIBLE);
     }
 }
